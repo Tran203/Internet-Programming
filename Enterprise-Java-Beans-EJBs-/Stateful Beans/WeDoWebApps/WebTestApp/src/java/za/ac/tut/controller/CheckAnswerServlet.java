8ed3105d175cacbe112fadeb71ad095aaadd1753
Tran203 <tshetshe203@gmail.com>
2023-04-28 01:02:51 +0200

    update

---
+++ Enterprise-Java-Beans-EJBs-/Stateful Beans/WeDoWebApps/WebTestApp/src/java/za/ac/tut/controller/CheckAnswerServlet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import za.ac.tut.model.QuestionPaper;
+
+/**
+ *
+ * @author Student
+ */
+public class CheckAnswerServlet extends HttpServlet {
+@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //session
+        HttpSession session = request.getSession(true);
+        
+        //get user Data
+        String testOption = request.getParameter("testOption");
+        Integer  numQuestions = (Integer)session.getAttribute("numQuestions");
+        Integer  totQuestions = (Integer)session.getAttribute("totQuestions");
+        
+        //call for help
+        QuestionPaper help = new QuestionPaper();
+        String question = help.getQuestion(session, testOption);
+        numQuestions++;
+        
+        //pass into session
+        session.setAttribute("testOption", testOption);
+        session.setAttribute(" numQuestions",numQuestions);
+        request.setAttribute("question", question);
+        
+        //navigation
+        if(numQuestions == totQuestions){
+            RequestDispatcher disp = request.getRequestDispatcher("outcome2.jsp");
+            disp.forward(request, response);
+        }else{
+            RequestDispatcher disp = request.getRequestDispatcher("ouctome.jsp");
+            disp.forward(request, response);
+        }
+    }
+}
