73042d395d9eac0764955b6789f15ae5693b55ba
Tran203 <tshetshe203@gmail.com>
2023-04-27 15:00:17 +0200

    update

---
+++ Enterprise-Java-Beans-EJBs-/Stateless Beans/CurrencyConvertorEJBModule/src/java/za/ac/tut/ejb/bl/CurrencyConvertorSB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.ejb.bl;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Student
+ */
+@Stateless
+public class CurrencyConvertorSB implements CurrencyConvertorSBLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
