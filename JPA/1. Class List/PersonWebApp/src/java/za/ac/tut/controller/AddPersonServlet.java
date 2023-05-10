3ee752933561d16e1629eed75e57cd8677fa7771
Tran203 <tshetshe203@gmail.com>
2023-05-10 22:48:35 +0200

    update

--- JPA/1. Class List/PersonWebApp/src/java/za/ac/tut/controller/AddPersonServlet.java
+++ JPA/1. Class List/PersonWebApp/src/java/za/ac/tut/controller/AddPersonServlet.java
@@ -7,17 +7,22 @@ package za.ac.tut.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import za.ac.tut.bl.PersonFacadeLocal;
+import za.ac.tut.entities.Person;
 
 /**
  *
  * @author Student
  */
 public class AddPersonServlet extends HttpServlet {
+    @EJB
+    private PersonFacadeLocal pfl;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -25,8 +30,23 @@ public class AddPersonServlet extends HttpServlet {
         String name =  request.getParameter("name");
         String surname = request.getParameter("surname");
         String gender = request.getParameter("gender");
+        
+        //create a person
+        Person person = createPerson(name,surname,gender);
+        
         //request
         RequestDispatcher disp=  request.getRequestDispatcher("");
         disp.forward(request, response);
+    }
+
+    private Person createPerson(String name, String surname, String gender) {
+        Person p =new Person();
+        
+        //set
+        p.setName(name);
+        p.setSurname(surname);
+        p.setSex(gender);
+        
+        return p;
     }
 }
