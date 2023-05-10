dc46554b435509be918b4bc7e719f8b4bc791b8f
Tran203 <tshetshe203@gmail.com>
2023-05-10 22:52:56 +0200

    Update SearchPersonServlet.java

--- JPA/1. Class List/PersonWebApp/src/java/za/ac/tut/controller/SearchPersonServlet.java
+++ JPA/1. Class List/PersonWebApp/src/java/za/ac/tut/controller/SearchPersonServlet.java
@@ -7,22 +7,30 @@ package za.ac.tut.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import za.ac.tut.bl.PersonFacadeLocal;
+import za.ac.tut.entities.Person;
 
 /**
  *
  * @author Student
  */
 public class SearchPersonServlet extends HttpServlet {
+    @EJB
+    private PersonFacadeLocal pfl;
 @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //get user data
-        String id =  request.getParameter("id");
+        Long id =  Long.parseLong(request.getParameter("id"));
+        //get the person
+        Person person = pfl.find(id);
+        request.setAttribute("person", person);
         
         //request
         RequestDispatcher disp=  request.getRequestDispatcher("");
