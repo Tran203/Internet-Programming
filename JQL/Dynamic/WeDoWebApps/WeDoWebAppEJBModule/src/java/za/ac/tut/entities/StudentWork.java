b9182dc4bfc4b720de09d2bb699cf6a4bfbcfc5a
Tran203 <tshetshe203@gmail.com>
2023-05-11 23:46:38 +0200

    update

---
+++ JQL/Dynamic/WeDoWebApps/WeDoWebAppEJBModule/src/java/za/ac/tut/entities/StudentWork.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Student
+ */
+@Entity
+public class StudentWork implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Integer qNum;
+    private String question;
+    private Integer userAnswer;
+    private Integer answer;
+    private String result;
+
+    public StudentWork() {
+    }
+
+    public StudentWork(Long id, Integer qNum, String question, Integer userAnswer, Integer answer, String result) {
+        this.id = id;
+        this.qNum = qNum;
+        this.question = question;
+        this.userAnswer = userAnswer;
+        this.answer = answer;
+        this.result = result;
+    }
+
+    public Integer getqNum() {
+        return qNum;
+    }
+
+    public void setqNum(Integer qNum) {
+        this.qNum = qNum;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Integer getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(Integer userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    public Integer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Integer answer) {
+        this.answer = answer;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+    
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof StudentWork)) {
+            return false;
+        }
+        StudentWork other = (StudentWork) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.ac.tut.entities.StudentWork[ id=" + id + " ]";
+    }
+    
+}
