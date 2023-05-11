3199f84bdda896f54f0da6f313344b7fbb6bff74
Tran203 <tshetshe203@gmail.com>
2023-05-11 17:15:47 +0200

    Update GetTestPerformanceServlet.java

--- JQL/Dynamic/1. TestPerformance/TestPerformanceWebApp/src/java/za/ac/tut/controller/GetTestPerformanceServlet.java
+++ JQL/Dynamic/1. TestPerformance/TestPerformanceWebApp/src/java/za/ac/tut/controller/GetTestPerformanceServlet.java
@@ -22,8 +22,10 @@ import za.ac.tut.entities.Student;
  * @author Student
  */
 public class GetTestPerformanceServlet extends HttpServlet {
-@EJB
+
+    @EJB
     private StudentFacadeLocal sfl;
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -34,21 +36,36 @@ public class GetTestPerformanceServlet extends HttpServlet {
         Double lowestMark = sfl.getLowestMark();
         Double highestMark = sfl.getHighestMArk();
         Double avgMark = sfl.getAvgMark();
-        
+
         //countable data
-        Integer cntPassed =sfl.cntAllPassed();
+        Integer cntPassed = sfl.cntAllPassed();
         Integer cntFailed = sfl.cntAllFailed();
-        
+
         Integer cntMales = sfl.cntAllMaleStd();
         Integer cntFemales = sfl.cntAllFemaleStd();
-        
+
         //passe && failed data
         Integer cntFailedMale = sfl.cntFailedAllMaleStd();
         Integer cntPassedMale = sfl.cntPassedAllMaleStd();
-        
+
         Integer cntFailedFemale = sfl.cntFailedAllFemaleStd();
         Integer cntPassedFemale = sfl.cntPassedAllFemaleStd();
-        
+
+        //pass back to user
+        request.setAttribute("list", list);
+        request.setAttribute("totInClass", totInClass);
+        request.setAttribute("lowestMark", lowestMark);
+        request.setAttribute("highestMark", highestMark);
+        request.setAttribute("avgMark", avgMark);
+        request.setAttribute("cntPassed", cntPassed);
+        request.setAttribute("cntFailed ", cntFailed);
+        request.setAttribute("cntMales", cntMales);
+        request.setAttribute("cntFemales", cntFemales);
+        request.setAttribute("cntFailedMale", cntFailedMale);
+        request.setAttribute("cntPassedMale", cntPassedMale);
+        request.setAttribute("cntFailedFemale", cntFailedFemale);
+        request.setAttribute("cntPassedFemale", cntPassedFemale);
+
         //reuww
         RequestDispatcher disp = request.getRequestDispatcher("./outcome/class_performance_outcome.jsp");
         disp.forward(request, response);
