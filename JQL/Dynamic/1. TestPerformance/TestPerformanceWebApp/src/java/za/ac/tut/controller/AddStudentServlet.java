a7ea55389f481f38d7072db0db9b32a9a999911d
Tran203 <tshetshe203@gmail.com>
2023-05-11 16:43:04 +0200

    Update AddStudentServlet.java

--- JQL/Dynamic/1. TestPerformance/TestPerformanceWebApp/src/java/za/ac/tut/controller/AddStudentServlet.java
+++ JQL/Dynamic/1. TestPerformance/TestPerformanceWebApp/src/java/za/ac/tut/controller/AddStudentServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import za.ac.tut.bl.StudentFacadeLocal;
+import za.ac.tut.entities.Student;
 
 /**
  *
@@ -25,10 +26,34 @@ public class AddStudentServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //
+        //collect data
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String gender = request.getParameter("gender");
+        Integer age = Integer.parseInt(request.getParameter("age"));
+        Double mark = Double.parseDouble(request.getParameter("mark"));
         
+        //create std
+        Student std = createStudent(name,surname,gender,age,mark);
+        sfl.create(std);
+        
+       
         //reuww
         RequestDispatcher disp = request.getRequestDispatcher("");
         disp.forward(request, response);
+    }
+
+    private Student createStudent(String name, String surname, String gender, Integer age, Double mark) {
+        //
+        Student std = new Student();
+        
+        //set
+        std.setName(name);
+        std.setSurname(surname);
+        std.setGender(gender);
+        std.setAge(age);
+        std.setPercMark(mark);
+        
+        return std;
     }
 }
