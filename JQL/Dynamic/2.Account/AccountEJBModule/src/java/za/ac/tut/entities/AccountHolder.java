a061b2d8e6353dc9827c5d4a74ff5e8ce451d079
Tran203 <tshetshe203@gmail.com>
2023-05-17 18:05:30 +0200

    update

---
+++ JQL/Dynamic/2.Account/AccountEJBModule/src/java/za/ac/tut/entities/AccountHolder.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Student
+ */
+@Entity
+public class AccountHolder implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name="ID_Num")
+    private Long id;
+    private String name;
+    private String surname;
+    private String gender;
+    private Integer age;
+    private Date dateOfBirth;
+    private String status;
+
+    public AccountHolder() {
+    }
+
+    public AccountHolder(Long id, String name, String surname, String gender, Integer age, Date dateOfBirth, String status) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AccountHolder)) {
+            return false;
+        }
+        AccountHolder other = (AccountHolder) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.ac.tut.entities.AccountHolder[ id=" + id + " ]";
+    }
+    
+}
