a6f185a5655506563494478e53c743933e6745ca
Tran203 <tshetshe203@gmail.com>
2023-05-30 22:46:39 +0200

    update

--- Exam Practise/Practise/Web/src/java/za/ac/tut/controller/AddItemServlet.java
+++ Exam Practise/Practise/Web/src/java/za/ac/tut/controller/AddItemServlet.java
@@ -6,27 +6,57 @@
 package za.ac.tut.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import za.ac.tut.bl.ItemFacadeLocal;
+import za.ac.tut.entities.Item;
 
 /**
  *
  * @author Student
  */
 public class AddItemServlet extends HttpServlet {
- @Override
+
+    @EJB
+    private ItemFacadeLocal ifl;
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //session
         HttpSession session = request.getSession(true);
+
+        //get item data
+        String code = request.getParameter("code");
+        String desc = request.getParameter("desc");
+        Double price = Double.parseDouble(request.getParameter("price"));
+        Integer qty = Integer.parseInt(request.getParameter("qty"));
+
+        //create item
+        Item item = createItem(code, desc, price, qty);
         
+        //add to database
+        ifl.create(item);
+
         //requst Dispatcher
-        RequestDispatcher disp = request.getRequestDispatcher("");
+        RequestDispatcher disp = request.getRequestDispatcher("add_item_outcome.jsp");
         disp.forward(request, response);
+    }
+
+    private Item createItem(String code, String desc, Double price, Integer qty) {
+        Item i = new Item();
+        
+        //set
+        i.setCode(code);
+        i.setDesc(desc);
+        i.setUnitPrice(price);
+        i.setQty(qty);
+        
+        return i;
     }
 }
---
+++ Exam Practise/Practise/Web/web/add_item_outcome.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : add_item_outcome
+    Created on : 30 May 2023, 10:43:56 PM
+    Author     : Student
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello Manager</h1>
+        <p>Item was added.</p>
+        <br><br>
+        <p>
+            <ul>
+                <li>Click <a href="add_item.jsp">here</a> to add Item</li>
+                <li>Click <a href="manager_menu.jsp">here</a> to access manager menu</li>
+                <li>Click  <a href="menu.jsp">here</a> to return to main page.</li>
+            </ul>
+        </p>
+</body>
+</html>
