ae5a5202e087538b1ecc3a5eae644d3228b29586
Tran203 <tshetshe203@gmail.com>
2023-05-31 14:28:36 +0200

    UPDATE

--- Exam Practise/Practise/Web/src/java/za/ac/tut/controller/ConfirmOrderServlet.java
+++ Exam Practise/Practise/Web/src/java/za/ac/tut/controller/ConfirmOrderServlet.java
@@ -7,26 +7,73 @@ package za.ac.tut.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import za.ac.tut.bl.Customer_OrderFacadeLocal;
+import za.ac.tut.entities.Address;
+import za.ac.tut.entities.Customer_Order;
+import za.ac.tut.entities.Item;
 
 /**
  *
  * @author Student
  */
 public class ConfirmOrderServlet extends HttpServlet {
-@Override
+
+    @EJB
+    private Customer_OrderFacadeLocal cfl;
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //session
         HttpSession session = request.getSession(true);
-        
+
+        //get addres
+        String street = request.getParameter("street");
+        String area = request.getParameter("area");
+        String code = request.getParameter("code");
+
+        //prepare to create order
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        Double tot = (Double) session.getAttribute("tot");
+        Address add = createAddress(street, area, code);
+
+        //create order
+        Customer_Order o = createOrder(cart, tot);
+        cfl.create(o);
+
         //requst Dispatcher
-        RequestDispatcher disp = request.getRequestDispatcher("index.html");
+        RequestDispatcher disp = request.getRequestDispatcher("order_outcome.jsp");
         disp.forward(request, response);
+    }
+
+    private Address createAddress(String street, String area, String code) {
+        Address a = new Address();
+
+        //set
+        a.setStreet(street);
+        a.setArea(area);
+        a.setCode(code);
+
+        return a;
+    }
+
+    private Customer_Order createOrder(List<Item> cart, Double tot) {
+        Customer_Order o = new Customer_Order();
+
+        //set
+        o.setCart(cart);
+        o.setTotValue(tot);
+        o.setCreationDate(new Date());
+
+        return o;
     }
 }
---
+++ Exam Practise/Practise/Web/web/order_outcome.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : order_outcome
+    Created on : 31 May 2023, 2:24:37 PM
+    Author     : Student
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Order Outcome</h1>
+        <p>The order was created</p>
+        <p>
+            Please click <a href="EndSessionServlet.do">here</a> to return to main page
+        </p>
+    </body>
+</html>
